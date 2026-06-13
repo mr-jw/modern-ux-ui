0d70d45744f981327c9a09d3ffe8722640a7deab
Joshua <44968962+mr-jw@users.noreply.github.com>
2026-06-13 22:11:02 +0100

    feat: (Navbar) links & mobile menu view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,62 @@
-export default function Navbar() {
-    return <nav className="fixed top-0 w-full z-500 transition-all duration-300 bg-green-950/20 backdrop-blur-sm">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="flex justify-between items-center h-14 sm:h-16 md:h-20">
-                <div>
-                    <img 
-                        src="/logo.png" 
-                        alt="CodeFlow" 
-                        className="w-6 h-6 sm:w-8 sm:h-8"
-                    />
-                </div>
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
 
-                <span>
-                    <span className="text-white">Code</span>
-                    <span className="text-blue-400">Flow</span>
-                </span>
+export default function Navbar() {
+    const [mobileMenuIsOpen, setMobileMenuIsOpen] = useState(false);
+
+    return (
+      <nav className="fixed top-0 w-full z-500 transition-all duration-300 bg-green-950/20 backdrop-blur-sm">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex justify-between items-center h-14 sm:h-16 md:h-20">
+            <div className="flex items-cemter space-x-1 group cursor-pointer">
+              <div>
+                <img
+                  src="/logo.png"
+                  alt="CodeFlow"
+                  className="w-6 h-6 sm:w-8 sm:h-8"
+                />
+              </div>
+
+              <span className="text-lg sm:text-xl md:text-2xl font-medium">
+                <span className="text-white">Code</span>
+                <span className="text-blue-400">Flow</span>
+              </span>
             </div>
+
+            {/* nav links */}
+            <div className="hidden md:flex items-center space-x-6">
+              <a
+                href="#features"
+                className="text-gray-300 hover:text-white text-sm lg:text-base"
+              >
+                Features
+              </a>
+              <a
+                href="#pricing"
+                className="text-gray-300 hover:text-white text-sm lg:text-base"
+              >
+                Pricing
+              </a>
+              <a
+                href="#testimonials"
+                className="text-gray-300 hover:text-white text-sm lg:text-base"
+              >
+                Testimonials
+              </a>
+            </div>
+
+            <button
+              className="md:hidden items-center p-2 text-grey-300 hover:text-white"
+              onClick={() => setMobileMenuIsOpen((prev) => !prev)}
+            >
+              {mobileMenuIsOpen ? (
+                <X />
+              ) : (
+                <Menu className="w-5 h-5 sm:w-6 sm:h-6" />
+              )}
+            </button>
+          </div>
         </div>
-    </nav>
+      </nav>
+    );
 }
